77506c544e2983ea531fd68cb904a3f90762e87a
ripon <riponhossainchopol@gmail.com>
2018-03-01 20:08:14 +0600

    Add files via upload

---
+++ functions-admin.php
@@ -0,0 +1,9 @@
+<?php
+function general_ripon(){
+    add_settings_field('theme_options','enter your name','ripon_test','general');
+    register_setting('general','theme_options');
+}
+add_action('admin_init','general_ripon');
+function ripon_test(){
+    echo '<input type="text" class="widefat" name="theme_options" value="'.get_option('theme_options').'" />';
+}
