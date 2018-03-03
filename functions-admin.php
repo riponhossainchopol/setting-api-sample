c99f61cc19174b215167131246c3d8960b85b6b4
ripon <riponhossainchopol@gmail.com>
2018-03-03 11:24:22 +0600

    Update functions-admin.php

--- functions-admin.php
+++ functions-admin.php
@@ -1,11 +1,41 @@
 <?php
-function general_ripon(){
-    add_settings_field('theme_options','enter your name','ripon_test','general');
-    register_setting('general','theme_options');
+function add_field_in_setting_options(){
+add_settings_section('theme_section','theme  options','dami_text','theme_slug');
+    
+    add_settings_field('general_setting','theme options','test_design','theme_slug','theme_section');
+    register_setting('theme_section','general_setting');
+    
+    
+    add_settings_field('wp_general','your company slogan','wp_callback','theme_slug','theme_section');
+    register_setting('theme_section','wp_general');
 }
-add_action('admin_init','general_ripon');
-function ripon_test(){
-    echo '<input type="text" class="widefat" name="theme_options" value="'.get_option('theme_options').'" />';
+add_action('admin_init','add_field_in_setting_options');
+function dami_text(){
+    echo ' ';
 }
-//index.php
-<?php echo get_option('theme_options')?>
+function test_design(){
+echo'<input class="widefat" type="text" name="general_setting" value="'.get_option('general_setting').'">';
+}
+function wp_callback(){
+    echo '<input type="text" class="widefat" name="wp_general" value="'.get_option('wp_general').'">';
+}
+//ADMIN MENU GENERAE
+function add_menu_in_admin_panel(){
+    add_menu_page('theme_options','theme option','manage_options','theme_slug','menu_design','',1);
+}
+add_action('admin_menu','add_menu_in_admin_panel');
+function menu_design(){
+    ?>
+    <?php settings_errors()?>
+    <form action="options.php" method="post">
+    <?php
+    do_settings_sections('theme_slug');
+    settings_fields('theme_section');
+    submit_button();
+    ?>
+    </form>
+<?php
+}
+// OUTPUT
+ <h1><?php echo get_option('general_setting')?></h1> 
+  <h1><?php echo get_option('wp_general')?></h1> 
